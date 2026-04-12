5e0fd22c2844eac199691b70117ff8b972214f51
Mark Lagodych <lgd.mrk@gmail.com>
2026-04-12 16:18:56 +0200

    Impl remaining integer utils

--- int/src/interpreter/Builtin/IntegerClass.php
+++ int/src/interpreter/Builtin/IntegerClass.php
@@ -6,6 +6,7 @@ namespace IPP\Interpreter\Builtin;
 
 use IPP\Interpreter\{Scope, SolClass, SolObject};
 use IPP\Interpreter\Builtin\{BuiltinMethod, BuiltinClass};
+use IPP\Interpreter\Exception\{ErrorCode, InterpreterError};
 
 class IntegerClass extends BuiltinClass
 {
@@ -15,15 +16,15 @@ class IntegerClass extends BuiltinClass
 
         $this->methods = [
             'isNumber' => new BuiltinMethod(fn($args) => $this->getBuiltinObject('true')),
-            'asString' => new BuiltinMethod(function ($args) {
-                $self = $args[0];
-                /** @var number */
-                $num = $self->internalAttribute;
-
-                $result = $this->getBuiltinObject('String')->send('new');
-                $result->internalAttribute = (string)$num;
-                return $result;
-            }),
+            'asString' => new BuiltinMethod(fn($args) => $this->asString($args)),
+            'asInteger' => new BuiltinMethod(fn($args) => $args[0]),
+            'equalTo:' => new BuiltinMethod(fn($args) => $this->equalTo($args)),
+            'greaterThan:' => new BuiltinMethod(fn($args) => $this->greaterThan($args)),
+            'plus:' => new BuiltinMethod(fn($args) => $this->plus($args)),
+            'minus:' => new BuiltinMethod(fn($args) => $this->minus($args)),
+            'multiplyBy:' => new BuiltinMethod(fn($args) => $this->multiplyBy($args)),
+            'divBy:' => new BuiltinMethod(fn($args) => $this->dividedBy($args)),
+            'timesRepeat:' => new BuiltinMethod(fn($args) => $this->timesRepeat($args)),
         ];
 
         $this->staticMethods = [
@@ -33,5 +34,143 @@ class IntegerClass extends BuiltinClass
                 return $str;
             }),
         ];
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function asString(array $args): SolObject
+    {
+        $self = $args[0];
+
+        $result = $this->getBuiltinObject('String')->send('new');
+        $result->internalAttribute = (string)$self->internalAttribute;
+        return $result;
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function equalTo(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('false');
+        }
+
+        return $self->internalAttribute === $other->internalAttribute
+            ? $this->getBuiltinObject('true')
+            : $this->getBuiltinObject('false');
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function greaterThan(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('false');
+        }
+
+        return $self->internalAttribute > $other->internalAttribute
+            ? $this->getBuiltinObject('true')
+            : $this->getBuiltinObject('false');
+    }
+
+    /**
+    * @param array<SolObject> $args
+    */
+    private function plus(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $result = new SolObject($this);
+        $result->internalAttribute = $self->internalAttribute + $other->internalAttribute;
+        return $result;
+    }
+
+    /**
+    * @param array<SolObject> $args
+    */
+    private function minus(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $result = new SolObject($this);
+        $result->internalAttribute = $self->internalAttribute - $other->internalAttribute;
+        return $result;
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function multiplyBy(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $result = new SolObject($this);
+        $result->internalAttribute = $self->internalAttribute * $other->internalAttribute;
+        return $result;
+    }
+
+    /**
+    * @param array<SolObject> $args
+    */
+    private function dividedBy(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        if ($other->internalAttribute === 0) {
+            throw new InterpreterError(ErrorCode::INT_INVALID_ARG, "Division by zero");
+        }
+
+        $result = new SolObject($this);
+        $result->internalAttribute = intdiv($self->internalAttribute, $other->internalAttribute);
+        return $result;
+    }
+
+    /**
+    * @param array<SolObject> $args
+    */
+    private function timesRepeat(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        $lastValue = $this->getBuiltinObject('nil');
+
+        for ($i = 1; $i <= $self->internalAttribute; $i++) {
+            $iObject = new SolObject($this);
+            $iObject->internalAttribute = $i;
+
+            $lastValue = $other->send('value', [$iObject]);
+        }
+
+        return $lastValue;
     }
 }
