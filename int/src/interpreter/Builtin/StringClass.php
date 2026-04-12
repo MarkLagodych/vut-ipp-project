9d1060a5f499d02fd0fb867acfda28d744ad0816
Mark Lagodych <lgd.mrk@gmail.com>
2026-04-12 16:00:36 +0200

    Impl remaining String utils, remove TODO comment from PHP code sniffer config

--- int/src/interpreter/Builtin/StringClass.php
+++ int/src/interpreter/Builtin/StringClass.php
@@ -21,6 +21,10 @@ class StringClass extends BuiltinClass
             'asString' => new BuiltinMethod(fn($args) => $args[0]),
             'print' => new BuiltinMethod(fn($args) => $this->print($args)),
             'equalTo:' => new BuiltinMethod(fn($args) => $this->equalTo($args)),
+            'asInteger' => new BuiltinMethod(fn($args) => $this->asInteger($args)),
+            'concatenateWith:' => new BuiltinMethod(fn($args) => $this->concatenateWith($args)),
+            'startsWith:endsBefore:' => new BuiltinMethod(fn($args) => $this->substring($args)),
+            'length' => new BuiltinMethod(fn($args) => $this->length($args)),
         ];
 
         $this->staticMethods = [
@@ -64,12 +68,103 @@ class StringClass extends BuiltinClass
         $other = $args[1];
 
         if (!$other->isInstanceOf($this)) {
-            /** @var SolObject */
             return $this->getBuiltinObject('false');
         }
 
         return $self->internalAttribute === $other->internalAttribute
             ? $this->getBuiltinObject('true')
             : $this->getBuiltinObject('false');
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function asInteger(array $args): SolObject
+    {
+        $self = $args[0];
+
+        $int = filter_var(
+            $self->internalAttribute,
+            FILTER_VALIDATE_INT,
+            FILTER_NULL_ON_FAILURE
+        );
+
+        if ($int === null) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $result = $this->getBuiltinObject('Integer')->send('new');
+        $result->internalAttribute = $int;
+        return $result;
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function concatenateWith(array $args): SolObject
+    {
+        $self = $args[0];
+        $other = $args[1];
+
+        if (!$other->isInstanceOf($this)) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $result = $this->getBuiltinObject('String')->send('new');
+        $result->internalAttribute = $self->internalAttribute . $other->internalAttribute;
+        return $result;
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function substring(array $args): SolObject
+    {
+        $self = $args[0];
+        $startIndex = $args[1]; // 1-based
+        $beforeIndex = $args[2]; // 1-based, the substring ends before this index
+
+        if (
+            !$startIndex->isInstanceOf($this->getBuiltinClass('Integer')) ||
+            !$beforeIndex->isInstanceOf($this->getBuiltinClass('Integer'))
+        ) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        $source = $self->internalAttribute;
+        $startIndex = $startIndex->internalAttribute - 1; // make 0-based
+        $beforeIndex = $beforeIndex->internalAttribute - 1; // make 0-based
+
+        if ($startIndex < 0 || $beforeIndex < 0) {
+            return $this->getBuiltinObject('nil');
+        }
+
+        if ($beforeIndex <= $startIndex || $startIndex >= strlen($source)) {
+            return $this->new();
+        }
+
+        if ($beforeIndex > strlen($source)) {
+            $beforeIndex = strlen($source);
+        }
+
+        $len = $beforeIndex - $startIndex;
+
+        $result = $this->getBuiltinObject('String')->send('new');
+        $result->internalAttribute = substr($source, $startIndex, $len);
+        return $result;
+    }
+
+    /**
+     * @param array<SolObject> $args
+     */
+    private function length(array $args): SolObject
+    {
+        $self = $args[0];
+
+        $length = strlen($self->internalAttribute);
+
+        $result = $this->getBuiltinObject('Integer')->send('new');
+        $result->internalAttribute = $length;
+        return $result;
     }
 }
